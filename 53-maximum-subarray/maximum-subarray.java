5dbcad760bfc99c511962a3cdaf4477ceb60aff5
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2026-07-17 11:48:14 +0530

    Time: 2 ms (4.35%) | Memory: 79.2 MB (11.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -7,19 +7,10 @@ class Solution {
             int d = Math.max(c , nums[0]+nums[1]);
             return d;
         }
-        // int sum = 0;
         dp[0]=nums[0];
-        // int sum = nums[0]+nums[1];
-        // int e = Math.max(sum , nums[0]);
-        // int f = Math.max(e , nums[1]);
-        // dp[1] = f;
-        // sum=nums[1];
-        // if(sum<0) sum=0;
+        ;
 
         for(int i=1 ; i<nums.length ; i++){
-            // if(sum<0) sum=0;
-            // sum = sum+nums[i];
-            // if(i==nums.length - 1 && sum<0) return Math.max(dp[i-1] , nums[nums.length - 1]);
             dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
         }
         int max= Integer.MIN_VALUE;
