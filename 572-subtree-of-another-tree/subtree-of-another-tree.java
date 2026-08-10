6092cb53004524469ad4bac836c3a23ebb43c9d9
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2026-08-10 20:06:17 +0530

    Time: 4 ms (19.37%) | Memory: 46.1 MB (94.97%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean sameTree(TreeNode root , TreeNode subRoot){
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+        if(root.val != subRoot.val) return false;
+        boolean left = sameTree(root.left , subRoot.left);
+        boolean right = sameTree(root.right , subRoot.right);
+        return left && right;
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+        if(sameTree(root , subRoot) == true) return true;
+        else{
+            if(sameTree(root.left , subRoot) == true) return true;
+            else{
+                if(sameTree(root.right , subRoot) == true) return true;
+                else{
+                    boolean left = isSubtree(root.left , subRoot);
+                    boolean right = isSubtree(root.right , subRoot);
+                    return left || right;
+                }
+            }
+        }
+    }
+}
