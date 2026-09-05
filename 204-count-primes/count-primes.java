eca6088ff4e9620819f10d751f9c6f39431d261c
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2026-09-05 11:59:54 +0530

    Time: 632 ms (25.28%) | Memory: 79.7 MB (19.83%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] a = new boolean[n];
+
+        for(int i = 2; i * i < n; i++){
+            if(!a[i]){
+                for(int j = i * i; j < n; j += i){
+                    a[j] = true;
+                }
+            }
+        }
+
+        int c = 0;
+
+        for(int i = 2; i < n; i++){
+            if(!a[i]) c++;
+        }
+
+        return c;
+    }
+}
