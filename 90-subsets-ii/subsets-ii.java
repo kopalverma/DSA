2813d2bc6c728413f319cc9b5a7a155338e2b794
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2026-09-15 23:31:27 +0530

    Time: 2 ms (99.72%) | Memory: 45.1 MB (42.07%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,29 +1,21 @@
 class Solution {
-    public List<List<Integer>> subset(int[] nums, int i, int j, List<Integer> at) {
-        List<List<Integer>> a = new ArrayList<>();
-
-        if (j == nums.length) {
-            a.add(new ArrayList<>(at));
-            return a;
+    public void subsets(int idx , int[] nums , List<Integer> at , List<List<Integer>> ans){
+        ans.add(new ArrayList<>(at));
+        if(idx == nums.length){
+            return;
         }
-
-        at.add(nums[j]);
-        a.addAll(subset(nums, i, j + 1, at));
-        at.remove(at.size() - 1);
-
-        a.addAll(subset(nums, i, j + 1, at));
-
-        return a;
+        for(int i = idx ; i<nums.length ; i++){
+            if(i!=idx && nums[i] == nums[i-1]) continue;
+            at.add(nums[i]);
+            subsets(i+1 , nums , at , ans);
+            at.remove(at.size() - 1);
+        }
     }
-
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> at = new ArrayList<>();
-        List<List<Integer>> a = subset(nums, 0, 0, at);
-
-        HashSet<List<Integer>> set = new HashSet<>(a);
-
-        return new ArrayList<>(set);
+        subsets(0 , nums , at , ans);
+        return ans;
     }
 }
