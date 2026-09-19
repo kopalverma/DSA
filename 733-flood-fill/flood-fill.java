d8cb66f850748543d331ceca8e1ebb92a1d260e0
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2026-09-19 20:13:09 +0530

    Time: 1 ms (39.47%) | Memory: 46.7 MB (61.60%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int initial = image[sr][sc];
+        int r = sr;
+        int c = sc;
+        boolean[][] visited = new boolean[image.length][image[0].length];
+
+        return floodFill(image, sr, sc, color, initial, visited, r, c);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color,
+                             int initial, boolean[][] visited, int r, int c) {
+
+        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length)
+            return image;
+
+        int m = image.length;
+        int n = image[0].length;
+
+        if(visited[sr][sc] || image[sr][sc] != initial)
+            return image;
+
+        image[sr][sc] = color;
+        visited[sr][sc] = true;
+
+        floodFill(image, sr + 1, sc, color, initial, visited, r, c);
+        floodFill(image, sr, sc + 1, color, initial, visited, r, c);
+        floodFill(image, sr - 1, sc, color, initial, visited, r, c);
+        floodFill(image, sr, sc - 1, color, initial, visited, r, c);
+
+        return image;
+    }
+}
